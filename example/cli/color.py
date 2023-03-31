5880c9eaf42d00a9217ddc9b36a01c227ef1ebb1
Aidan Melen <aidan-melen@protonmail.com>
2023-03-30 20:46:10 -0600

    fixed CursesColor and CursesColorPair classes

--- example/cli/color.py
+++ example/cli/color.py
@@ -8,7 +8,11 @@ def main(screen):
     curses_color.init_colors()
 
     curses_color_pair = CursesColorPair(curses_color)
-    curses_color_pair.init_color_pairs(bg_color_name="WHITE")
+    curses_color_pair.init_pairs()
+    curses_color_pair.init_pairs(bg_color_name="BLACK")
+    curses_color_pair.init_pairs(bg_color_name="WHITE")
+    curses_color_pair.init_pairs(bg_color_name="GOLD")
+    # curses_color_pair.init_pair("RED", bg_color_name="BLACK")
 
     # Turn off echoing and enable cbreak mode
     curses.noecho()
@@ -20,39 +24,29 @@ def main(screen):
     # Clear the screen
     screen.clear()
 
-    # screen.addstr(str(curses_color.color_name_to_number))
-    # screen.addstr(str(curses_color_pair.color_name_to_number))
-
     # Display all color pairs
-    for color_pair_name, _ in curses_color_pair:
-        screen.addstr(color_pair_name, curses_color_pair[color_pair_name] | curses.A_BOLD)
+    for color_pair_name, color_pair_number in curses_color_pair:
+        screen.addstr(f"█ {color_pair_name} █", curses_color_pair.get(color_pair_name))
     else:
         screen.addstr("\n\n")
 
     # Display specific color pairs
-    # screen.addstr("RED_ON_BLACK", curses_color_pair["RED_ON_BLACK"])
-    # screen.addstr("GREEN_ON_BLACK", curses_color_pair["GREEN_ON_BLACK"])
-    # screen.addstr("BLUE_ON_BLACK", curses_color_pair["BLUE_ON_BLACK"])
-    # screen.addstr("\n\n")
-
-    # Display custom color pairs by name
-    # curses_color_pair.init_color_pair_by_name("WHITE", "TURQUOISE")
-    # curses_color_pair.init_color_pair_by_name("BLACK", "GOLD")
-    # curses_color_pair.init_color_pair_by_name("NAVY", "WHITE")
-    # screen.addstr("WHITE_ON_TURQUOISE", curses_color_pair["WHITE_ON_TURQUOISE"])
-    # screen.addstr("BLACK_ON_GOLD", curses_color_pair["BLACK_ON_GOLD"])
-    # screen.addstr("NAVY_ON_WHITE", curses_color_pair["NAVY_ON_WHITE"] | curses.A_BOLD)
-    # screen.addstr("\n\n")
+    screen.addstr("RED_ON_BLACK", curses_color_pair["RED_ON_BLACK"])
+    screen.addstr("GREEN_ON_BLACK", curses_color_pair["GREEN_ON_BLACK"])
+    screen.addstr("BLUE_ON_BLACK", curses_color_pair["BLUE_ON_BLACK"])
+    screen.addstr("\n\n")
 
     # Display custom color pairs by number
-    # curses_color_pair.init_color_pair_by_number(curses_color["TURQUOISE"], curses_color["WHITE"])
-    # curses_color_pair.init_color_pair_by_number(curses_color["GOLD"], curses_color["WHITE"])
-    # curses_color_pair.init_color_pair_by_number(curses_color["BROWN"], curses_color["WHITE"])
-    # screen.addstr("TURQUOISE_ON_WHITE", curses_color_pair["WHITE_ON_TURQUOISE"])
+    # curses_color_pair.init_pair("BLACK")
+    # curses_color_pair.init_pair("WHITE")
+    # curses_color_pair.init_pair("WHITE", "BLACK")
+
+    # curses_color_pair.init_pair("GOLD", "WHITE")
+    # curses_color_pair.init_pair("BROWN", "WHITE")
+    # screen.addstr("TURQUOISE_ON_WHITE", curses_color_pair["TURQUOISE_ON_WHITE"])
     # screen.addstr("GOLD_ON_WHITE", curses_color_pair["GOLD_ON_WHITE"])
     # screen.addstr("BROWN_ON_WHITE", curses_color_pair["BROWN_ON_WHITE"] | curses.A_BOLD)
-
-    screen.addstr("\n\n")
+    # screen.addstr("\n\n")
     
     # Refresh the screen
     screen.refresh()
--- src/cli/curses_color.py
+++ src/cli/curses_color.py
@@ -7,6 +7,8 @@ class CursesColor:
     COLOR_DEFAULT = -1
 
     def __init__(self):
+        self._color_name_to_number = {}
+        self._used_color_numbers = set()
         self._color_name_to_rgb = {
             "WHITE": (1000, 1000, 1000),
             "BLACK": (0, 0, 0),
@@ -31,6 +33,7 @@ class CursesColor:
             "BEIGE": (1000, 1000, 750),
             "BROWN": (750, 500, 250),
             "IVORY": (1000, 1000, 750),
+            "SNOW": (1000, 950, 950),
             "TAN": (750, 500, 250),
             "PEACH": (1000, 854, 725),
             "LILAC": (860, 690, 1000),
@@ -55,16 +58,13 @@ class CursesColor:
             "OLIVE_GREEN": (333, 420, 65),
             "GRASS_GREEN": (0, 604, 90),
             "MAUVE": (880, 690, 690),
-            "PEACOCK": (51, 161, 201),
+            "PEACOCK": (0, 664, 717),
             "SIENNA": (640, 320, 160),
             "STRAW": (1000, 930, 700),
             "CINNAMON": (820, 410, 220),
             "MUSTARD": (1000, 860, 350),
             "PLATINUM": (229, 228, 226),
         }
-
-        self._color_name_to_number = {}
-        self._used_color_numbers = set()
 
     def start_color(self):
         curses.start_color()
@@ -86,50 +86,69 @@ class CursesColor:
     @property
     def color_name_to_number(self):
         return self._color_name_to_number
-    
+
     def color_content_by_name(self, color_name):
-        return curses.color_content(self._color_name_to_number.get(color_name))
-    
+        return curses.color_content(self._color_name_to_number[color_name])
+
     def color_content_by_number(self, color_number):
         return curses.color_content(color_number)
 
-    def __setitem__(self, color_name, rgb):
-        if color_name == "DEFAULT":
-            self._color_name_to_number[color_name] = self.COLOR_DEFAULT
-
-        num_of_predefined_colors = 8
-        for color_number in range(num_of_predefined_colors, curses.COLORS):
+    def next_color_number(self):
+        # use color numbers from highest to lowest to override the 8-bit color numbers last
+        for color_number in range(curses.COLORS-1, -1, -1):
             if color_number not in self._used_color_numbers:
-                curses.init_color(color_number, *rgb)
-                self._color_name_to_rgb[color_name] = rgb
-                self._color_name_to_number[color_name] = color_number
-                self._used_color_numbers.add(color_number)
-                break
+                return color_number
         else:
-            raise RuntimeError("All 256 colors are already set")
+            raise ValueError("Color is greater than 255 (curses.COLORS - 1).")
+
+    def is_color_initialized(self, color_number):
+        return color_number in self._color_name_to_number.values()
+
+    def __setitem__(self, color_name, rgb):
+        color_name = str(color_name).upper()
+        
+        if color_name in self._color_name_to_number:
+            # update existing color
+            self._color_name_to_rgb[color_name] = rgb
+        else:
+            # create new color
+            color_number = self.next_color_number()
+            self._color_name_to_number[color_name] = color_number
+            self._used_color_numbers.add(color_number)
+
+        curses.init_color(color_number, *rgb)
 
     def __getitem__(self, color_name):
-        if color_name == "DEFAULT":
-            return self.COLOR_DEFAULT
         return self._color_name_to_number[color_name]
-    
-    def get(self, color_name, default=None):
-        if color_name == "DEFAULT":
-            return self.COLOR_DEFAULT
+
+    def get(self, color_name, default=COLOR_DEFAULT):
         return self._color_name_to_number.get(color_name, default)
 
     def __iter__(self):
         return iter(self._color_name_to_number.items())
 
+    def items(self):
+        return self._color_name_to_number.items()
+
 
 class CursesColorPair:
-    def __init__(self, named_curses_color: Type[CursesColor]):
-        self._curses_color = named_curses_color
-        self._color_pair_name_to_number = {}
-        self._used_color_pair_numbers = set()
+    COLOR_PAIR_DEFAULT = 0
 
-    def init_color_pair_by_name(self, fg_color_name, bg_color_name="BLACK"):
-        pair_name = f"{fg_color_name}_ON_{bg_color_name}"
+    def __init__(self, curses_color: Type[CursesColor]):
+        self._curses_color = curses_color
+        self._pair_name_to_number = {}
+        self._used_pair_numbers = set()
+
+    def init_pairs(self, bg_color_name=None):
+        bg_color_name = str(bg_color_name).upper()
+        bg_color_number = self._curses_color.get(bg_color_name)
+
+        for fg_color_name, fg_color_number in self._curses_color:
+            self.init_pair(fg_color_name, bg_color_name)
+
+    def init_pair(self, fg_color_name=None, bg_color_name=None):
+        pair_name = f"{fg_color_name}_ON_{bg_color_name}".upper()
+
         fg_color_number = self._curses_color.get(fg_color_name)
         bg_color_number = self._curses_color.get(bg_color_name)
 
@@ -138,49 +157,40 @@ class CursesColorPair:
         
         if not bg_color_number:
             raise ValueError(f"The background color name {bg_color_name} has not been initialized.")
-        
-        pair_number = sum((fg_color_number, bg_color_number))
 
-        curses.init_pair(pair_number, self._curses_color[fg_color_name], self._curses_color[bg_color_name])
-        self._color_pair_name_to_number[pair_name] = curses.color_pair(pair_number)
-    
-    def init_color_pair_by_number(self, fg_color_number, bg_color_number):
-        color_number_to_name = {number: name for name, number in self._curses_color.color_name_to_number.items()}
-        fg_color_name = color_number_to_name.get(fg_color_number)
-        bg_color_name = color_number_to_name.get(bg_color_number)
+        self[pair_name] = (fg_color_name, bg_color_name)        
 
-        if not fg_color_name:
-            raise ValueError(f"The foreground color number {fg_color_number} has not been initialized.")
-        
-        if not bg_color_name:
-            raise ValueError(f"The background color number {bg_color_number} has not been initialized.")
-        
-        pair_name = f"{fg_color_name}_ON_{bg_color_name}"
-        pair_number = sum((fg_color_number, bg_color_number))
-
-        curses.init_pair(pair_number, self._curses_color[fg_color_name], self._curses_color[bg_color_name])
-        self._color_pair_name_to_number[pair_name] = curses.color_pair(pair_number)
-
-    def init_color_pairs(self, bg_color_name="DEFAULT"):
-        for fg_color_name, fg_color_number in self._curses_color:
-            pair_name = f"{fg_color_name}_ON_{bg_color_name}"
-            pair_number = sum((fg_color_number, self._curses_color[bg_color_name]))
-
-            curses.init_pair(pair_number, fg_color_number, self._curses_color[bg_color_name])
-            self._color_pair_name_to_number[pair_name] = curses.color_pair(pair_number)
+    def next_pair_number(self):
+        for pair_number in range(1, curses.COLOR_PAIRS - 1):
+            if pair_number not in self._used_pair_numbers:
+                return pair_number
+        else:
+            raise ValueError("Color pair is greater than 32765 (curses.COLOR_PAIRS - 1).")
 
     @property
-    def color_pairs_name_to_number(self):
-        return self._color_pair_name_to_number
+    def pair_name_to_number(self):
+        return self._pair_name_to_number
 
-    # def __setitem__(self, name, color_number):
-    #     # TODO
-    #     curses.init_pair(pair_number, self._curses_color[fg_color_name], self._curses_color[bg_color_name])
-    #     self._color_pair_name_to_number[pair_name] = curses.color_pair(pair_number)
-    #     return self._color_pair_name_to_number[name] = color_number
+    def __setitem__(self, pair_name, color_pair_names):
+        if len(color_pair_names) != 2:
+            raise ValueError("color_pair_names must be a 2-tuple of color names.")
+
+        fg_color_name, bg_color_name = map(str.upper, color_pair_names)
+        pair_number = self.next_pair_number()
+        curses.init_pair(
+            pair_number, self._curses_color.get(fg_color_name), self._curses_color.get(bg_color_name)
+        )
+        self._pair_name_to_number[pair_name] = curses.color_pair(pair_number)
+        self._used_pair_numbers.add(pair_number)
 
     def __getitem__(self, name):
-        return self._color_pair_name_to_number[name]
+        return self._pair_name_to_number[name]
     
+    def get(self, pair_name, default=COLOR_PAIR_DEFAULT):
+        return self._pair_name_to_number.get(pair_name, default)
+
     def __iter__(self):
-        return iter(self._color_pair_name_to_number.items())
+        return iter(self._pair_name_to_number.items())
+    
+    def items(self):
+        return self._pair_name_to_number.items()
