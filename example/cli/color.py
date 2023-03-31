c3b31427464f966f8ce6141d8c056750a2463dde
Aidan Melen <aidan-melen@protonmail.com>
2023-03-30 21:35:47 -0600

    added docstrings and type annotations to the CursesColor and CursesColorPair classes.

--- example/cli/color.py
+++ example/cli/color.py
@@ -11,8 +11,6 @@ def main(screen):
     curses_color_pair.init_pairs()
     curses_color_pair.init_pairs(bg_color_name="BLACK")
     curses_color_pair.init_pairs(bg_color_name="WHITE")
-    curses_color_pair.init_pairs(bg_color_name="GOLD")
-    # curses_color_pair.init_pair("RED", bg_color_name="BLACK")
 
     # Turn off echoing and enable cbreak mode
     curses.noecho()
@@ -29,24 +27,6 @@ def main(screen):
         screen.addstr(f"█ {color_pair_name} █", curses_color_pair.get(color_pair_name))
     else:
         screen.addstr("\n\n")
-
-    # Display specific color pairs
-    # screen.addstr("RED_ON_BLACK", curses_color_pair["RED_ON_BLACK"])
-    # screen.addstr("GREEN_ON_BLACK", curses_color_pair["GREEN_ON_BLACK"])
-    # screen.addstr("BLUE_ON_BLACK", curses_color_pair["BLUE_ON_BLACK"])
-    # screen.addstr("\n\n")
-
-    # Display custom color pairs by number
-    # curses_color_pair.init_pair("BLACK")
-    # curses_color_pair.init_pair("WHITE")
-    # curses_color_pair.init_pair("WHITE", "BLACK")
-
-    # curses_color_pair.init_pair("GOLD", "WHITE")
-    # curses_color_pair.init_pair("BROWN", "WHITE")
-    # screen.addstr("TURQUOISE_ON_WHITE", curses_color_pair["TURQUOISE_ON_WHITE"])
-    # screen.addstr("GOLD_ON_WHITE", curses_color_pair["GOLD_ON_WHITE"])
-    # screen.addstr("BROWN_ON_WHITE", curses_color_pair["BROWN_ON_WHITE"] | curses.A_BOLD)
-    # screen.addstr("\n\n")
     
     # Refresh the screen
     screen.refresh()
